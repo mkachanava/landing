bc2cac0d4bfedb8289659090470564844de624a1
a87dev <104082336+a87dev@users.noreply.github.com>
2022-07-18 19:03:27 +0300

    Added changes in menu burger

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const iconMenu = document.querySelector('.menu-icon');
-const menuBody = document.querySelector('.menu-body');
+const menuIcon = document.querySelector('#menuIcon');
+const headerMenu = document.querySelector('#headerMenu');
 
-if (iconMenu) {
-	iconMenu.addEventListener("click", function (f) {
-		document.body.classList.toggle('-lock');
-		iconMenu.classList.toggle('-active');
-		menuBody.classList.toggle('-active');
+if (menuIcon) {
+	menuIcon.addEventListener("click", function (f) {
+		menuIcon.classList.toggle('active');
+		headerMenu.classList.toggle('active');
 	});
 }
