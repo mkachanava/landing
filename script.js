ca758b4790eab8f7055a3023175e18e1f15e143f
a87dev <104082336+a87dev@users.noreply.github.com>
2022-07-29 09:54:06 +0300

    Added changes

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 const menuIcon = document.querySelector('#menuIcon');
-const headerMenu = document.querySelector('#headerMenu');
+const headerMenu = document.querySelector('#headerMenuMobile');
+const footer = document.querySelector('#footer');
+const main = document.querySelector('#main');
+
 
 if (menuIcon) {
 	menuIcon.addEventListener("click", function (f) {
 		menuIcon.classList.toggle('active');
 		headerMenu.classList.toggle('active');
+		main.classList.toggle('hide');
+		footer.classList.toggle('hide');
 	});
 }
+
+
+
+
+
+
+
+let popupContainer = document.querySelector('.popup-container');
+let popup = document.querySelector('.popup');
+let openPopup = document.querySelectorAll('.open-popup');
+let closePopup = document.querySelector('.close-popup');
+
+openPopup.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupContainer.classList.add('active');
+        popup.classList.add('active');
+    })
+});
+
+closePopup.addEventListener('click',() => {
+    popupContainer.classList.remove('active');
+    popup.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target === popupContainer) {
+        popupContainer.classList.remove('active');
+        popup.classList.remove('active');
+    }
+});
